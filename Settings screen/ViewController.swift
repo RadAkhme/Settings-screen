dffb1611bbf80eb46a4f057b19e2f84f161d6a1d
RadikAkhme <a.radik.s@ya.ru>
2022-08-30 14:29:26 +0300

    Initial Commit

---
+++ Settings screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings screen
+//
+//  Created by Радик Ахметзянов on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

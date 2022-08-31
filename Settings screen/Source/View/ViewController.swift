8e03ce656384101fd794340393961d21f5a11437
RadikAkhme <a.radik.s@ya.ru>
2022-08-31 23:44:33 +0300

    Add TableView and UITableViewDataSource, UITableViewDelegate.

--- Settings screen/Source/View/ViewController.swift
+++ Settings screen/Source/View/ViewController.swift
@@ -6,28 +6,117 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
-    // MARK: - Outlets
-
+    private var settingItems: [[SettingsItem]]?
     
+    // MARK: - Outlets
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.layer.masksToBounds = true
+        tableView.clipsToBounds = true
+        tableView.layer.cornerRadius = 10
+        tableView.register(UserTableViewCell.self, forCellReuseIdentifier: "user")
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        return tableView
+    }()
+    
+    private lazy var searchBar: UITextField = {
+        let searchBar = UITextField()
+        searchBar.backgroundColor = .systemGray5
+        searchBar.placeholder = "Поиск"
+        searchBar.textAlignment = .natural
+        searchBar.layer.cornerRadius = 10
+        searchBar.setLeftIcon(UIImage(systemName: "magnifyingglass")!)
+        return searchBar
+    }()
     
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        settingItems = SettingsItem.settingItems
+        view.backgroundColor = .systemGray6
+        title = "Настройки"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        setupHierarchy()
+        setupLayout()
     }
 
     // MARK: - Setup
 
     private func setupHierarchy() {
-        
+        view.addSubview(tableView)
+        view.addSubview(searchBar)
     }
     
     private func setupLayout() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(20)
+            make.right.equalTo(view)
+            make.bottom.equalTo(view)
+            make.left.equalTo(view)
+        }
         
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.centerX.equalTo(view)
+            make.left.equalTo(view).offset(15)
+            make.height.equalTo(35)
+        }
     }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 80
+            }
+        return UITableView.automaticDimension
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return settingItems?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingItems?[section].count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            let user = tableView.dequeueReusableCell(withIdentifier: "user", for: indexPath) as? UserTableViewCell
+            user?.setting = settingItems?[indexPath.section][indexPath.row]
+            user?.accessoryType = .disclosureIndicator
+            return user ?? UITableViewCell()
+        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TableViewCell
+        cell?.setting = settingItems?[indexPath.section][indexPath.row]
+        cell?.accessoryType = .disclosureIndicator
+        return cell ?? UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("Нажал: \(settingItems?[indexPath.section][indexPath.row].title ?? "")")
+    }
+    
+}
+
+extension UITextField {
+    func setLeftIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 8, y: 6, width: 20, height: 18))
+        iconView.image = image
+        iconView.tintColor = .lightGray
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
+}
